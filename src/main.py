b15fcfb514c6a54771c3e86f333415ba940324d7
Rafael Calai <rafaelcalai@hotmail.com>
2023-12-03 04:42:00 -0800

    Producer consumer created to controll the turtle

--- src/main.py
+++ src/main.py
@@ -1,77 +1,50 @@
-from action_recognition import ActionRecognition
-import getopt, sys
+from threading import *
+import time
+import queue
+from turtle_cmd import TurtleCmdPublisher, cmd
 
-def help(long_options):
-    for _ in range(25): print("-", end="")
-    print("\n\tHelp:")
-    for _ in range(25): print("-", end="")
-    print("\n\nArgument options:", long_options )
-    print("-h --Help:\t\t Print the help instructions.")
-    print("-v --Video:\t\t Opens a window with the video and point detections.")
-    print("-c --Collect:\t\t Collects n videos and save to be trained.")
-    print("-t --Training:\t\t Runs training of videos captured.")
-    print("-d --Detection:\t\t Runs real time detection.")
-    
+# ros2 run turtlesim turtlesim_node
 
+items=[]
+def produce(c):
+   while True:
+       item='S' #Step 1.2
+       print("Producer Producing Item:", item)
+       c.put(item)
+       print("Producer giving Notification")
+       time.sleep(2)
 
-    
+       item='F' #Step 1.2
+       c.put(item)
+       print("Producer giving Notification")
+       time.sleep(2)
+       item='L' #Step 1.2
+       c.put(item)
+       print("Producer giving Notification")
+       time.sleep(2)
+       item='L' #Step 1.2
+       c.put(item)
+       print("Producer giving Notification")
+       time.sleep(2)
+       item='R' #Step 1.2
+       c.put(item)
+       print("Producer giving Notification")
+       time.sleep(2)
 
-
-def main():
-    for _ in range(35): print("-", end="")
-    print("\nScript to Recognize Actions IACOS")
-    for _ in range(35): print("-", end="")
-
-     # Optionspython
-    options = "hvctd:"
-
-    # Long options
-    long_options = ["Help", "Video", "Collect", "Training", "Detection"] 
-
-    # total arguments
-    n = len(sys.argv)
-    print("\nTotal arguments passed:", n)
-    if(n == 1):
-        help(long_options)
-    
-    # Arguments passed
-    print("\nName of Python script:", sys.argv[0])
- 
-    print("\nArguments passed:", end = " ")
-    for i in range(1, n):
-        print(sys.argv[i], end = " ")
-
-    argumentList = sys.argv[1:] 
-    act = ActionRecognition()
-   
-
-    try:
-        # Parsing argument
-        arguments, _ = getopt.getopt(argumentList, options, long_options)
-        
-
-        # checking each argument
-        for currentArgument, currentValue in arguments:
-    
-            if currentArgument in ("-h", "--Help"):
-                help(long_options)
-                
-            elif currentArgument in ("-v", "--Video"):
-                act.video_detection()
-                
-            elif currentArgument in ("-c", "--Collect"):
-                act.collect_images()
-
-            elif currentArgument in ("-t", "--Training"):
-                act.training_model()
-
-            elif currentArgument in ("-d", "--Detect"):
-                act.real_time_detection()      
-                
-    except getopt.error as err:
-        # output error, and return with an error code
-        print (str(err))
+       item='S' #Step 1.2
+       c.put(item)
+       print("Producer giving Notification")
 
 
-if __name__ == "__main__":
-    main()
+def consume(c, args=None):
+    cmd(c)
+
+def main():    
+    q=queue.Queue()
+    t1=Thread(target=consume, args=(q,))
+    t2=Thread(target=produce, args=(q,))
+    t1.start()
+    t2.start()
+
+if __name__ == '__main__':
+      main()
--- src/turtle_cmd.py
+++ src/turtle_cmd.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 import rclpy
 from rclpy.node import Node
+import math
 
 from geometry_msgs.msg import Twist
-PI = 3.1415926535897
+PI = math.pi
 
 class TurtleCmdPublisher(Node):
 
-    def __init__(self):
+    def __init__(self, queue=None):
         super().__init__('turtle_cmd_publisher')
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        timer_period = 1  # seconds
+        self.cmd = 'S'
+        if queue:
+            self.queue = queue
+            self.timer = self.create_timer(timer_period, self.timer_callback2)
+        else:
+            self.timer = self.create_timer(timer_period, self.timer_callback)  
 
     def timer_callback(self):
         vel_msg = Twist()
@@ -22,6 +27,7 @@ class TurtleCmdPublisher(Node):
         print("Type L to turn left")
         print("Type R to turn right")
         print("Type F to go foward")
+        print("Type S to stop")
         cmd = input("command: ")
 
         #Converting from angles to radians
@@ -35,24 +41,62 @@ class TurtleCmdPublisher(Node):
         vel_msg.angular.y=0.0
         vel_msg.angular.z=0.0
     
-        if cmd == "R":
+        if cmd == "R" or cmd == "r":
             vel_msg.angular.z = -abs(relative_angle)
-        elif cmd == "L": 
+        elif cmd == "L" or cmd == "l":
             vel_msg.angular.z = abs(relative_angle)
-        elif cmd == "F":
-            vel_msg.linear.x=1.0
+        elif cmd == "S" or cmd == "s":
+            vel_msg.linear.x=0.0
+        else:    
+            vel_msg.linear.x=0.5
 
         self.publisher_.publish(vel_msg)
         self.get_logger().info('Publishing: "%s"' % vel_msg)
-        self.i += 1
+        self.i += 1    
+
+    def timer_callback2(self):
+        vel_msg = Twist()
 
 
-def cmd(args=None):
+        if not self.queue.empty():
+            self.cmd = str(self.queue.get())
+            self.get_logger().info("Command received: %s" % self.cmd)
+            
+
+        #Converting from angles to radians
+        relative_angle = 90*2*PI/360 # 90 degrees
+    
+        #Initialize variables
+        vel_msg.linear.x=0.0
+        vel_msg.linear.y=0.0
+        vel_msg.linear.z=0.0
+        vel_msg.angular.x=0.0
+        vel_msg.angular.y=0.0
+        vel_msg.angular.z=0.0
+
+
+        if self.cmd == 'R' or self.cmd == 'r':
+            vel_msg.angular.z = -abs(relative_angle)
+            self.cmd = 'F'
+        elif self.cmd == 'L' or self.cmd == 'l':
+            vel_msg.angular.z = abs(relative_angle)
+            self.cmd = 'F'
+        elif self.cmd == 'S' or self.cmd == 's':
+            vel_msg.linear.x=0.0
+        else:    
+            vel_msg.linear.x=0.5
+
+
+        self.publisher_.publish(vel_msg)
+        self.get_logger().info('Publishing command:"%s" "%s"' % (self.cmd, vel_msg))
+
+
+def cmd(queue=None,args=None):
     rclpy.init(args=args)
     node = rclpy.create_node('turtle_commander')
     node.get_logger().info('Created node')
 
-    turtle_cmd_publisher = TurtleCmdPublisher()
+    turtle_cmd_publisher = TurtleCmdPublisher(queue)
     
     rclpy.spin(turtle_cmd_publisher)
 
